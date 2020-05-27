56ebcc4336780e5180a1ee14896a7cf3ce3f8077
raphey <16931750+raphey@users.noreply.github.com>
2020-05-26 18:56:45 -0700

    generating random sequences

--- tone.py
+++ tone.py
@@ -1,11 +1,9 @@
 from functools import reduce
+from random import choice
+from random import randint
 
 from scipy.io.wavfile import write
 import numpy as np
-
-SAMPLE_RATE = 44100
-
-FREQUENCY = 440
 
 
 class Tone(object):
@@ -43,15 +41,62 @@ class ToneSequence(Tone):
         self.data = reduce(lambda a, b: np.concatenate((a, self.silence_data, b)), self.tones_data)
 
 
+class RandomTonePairSequence(ToneSequence):
+    note_frequencies = [
+        ('C3', 130.81),
+        ('C#3', 138.59),
+        ('D3', 146.83),
+        ('D#3', 155.56),
+        ('E3', 164.81),
+        ('F3', 174.61),
+        ('F#3', 185.00),
+        ('G3', 196.00),
+        ('G#3', 207.65),
+        ('A3', 220.00),
+        ('A#3', 233.08),
+        ('B3', 246.94),
+        ('C4', 261.63),
+        ('C#4', 277.18),
+        ('D4', 293.66),
+        ('D#4', 311.13),
+        ('E4', 329.63),
+        ('F4', 349.23),
+        ('F#4', 369.99),
+        ('G4', 392.00),
+        ('G#4', 415.30),
+        ('A4', 440.00),
+        ('A#4', 466.16),
+        ('B4', 493.88),
+        ('C5', 523.25),
+        ('C#5', 554.37),
+        ('D5', 587.33),
+        ('D#5', 622.25),
+        ('E5', 659.25),
+        ('F5', 698.46),
+        ('F#5', 739.99),
+        ('G5', 783.99),
+        ('G#5', 830.61),
+        ('A5', 880.00),
+    ]
+
+    semitone_ratio = 2.0**(1.0 / 12)
+
+    def __init__(self, tone_count, tuning_width, tone_durations=1, tone_amplitudes=0.5, gap_durations=1):
+        self.base_key, self.base_f = choice(list(self.note_frequencies))
+        self.tone_count = tone_count
+        self.tuning_width = tuning_width
+        self.random_bits = ''.join(str(randint(0, 1)) for _ in range(tone_count))
+        print(self.random_bits)
+        tuning_ratio = self.semitone_ratio**self.tuning_width
+        lo_f, hi_f = ((self.base_f, self.base_f * tuning_ratio) if self.random_bits[0] == '0' else
+                      (self.base_f / tuning_ratio, self.base_f))
+        frequencies = [lo_f if b == '0' else hi_f for b in self.random_bits]
+        super(RandomTonePairSequence, self).__init__(tone_frequencies=frequencies,
+                                                     tone_durations=tone_durations,
+                                                     tone_amplitudes=tone_amplitudes,
+                                                     gap_durations=gap_durations)
 
 
 
-
-test_tone = Tone(440)
-test_tone.write_tone_to_file('example2.wav')
-
-test_tone = Silence()
-test_tone.write_tone_to_file('example3.wav')
-
-test_sequence = ToneSequence([440, 450, 460, 470])
-test_sequence.write_tone_to_file('example4.wav')
+tps = RandomTonePairSequence(10, 0.15)
+tps.write_tone_to_file('example1.wav')
